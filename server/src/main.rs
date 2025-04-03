7741c23fba125999ab697032529927e819714de5
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2025-04-03 23:24:48 +0100

    Improve the sample code

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,17 +5,22 @@ use std::{
 use websocket::sync::Server;
 
 fn handle_client(mut stream: TcpStream) {
+    match stream.write_all(b"Connection success\n") {
+        Ok(s) => s,
+        Err(_) => return,
+    }
     loop {
         let mut buffer = [0; 1024];
         match stream.read(&mut buffer) {
             Ok(0) => break,
             Ok(n) => {
-                let state:String = String::from_utf8(buffer[..n].to_vec()).expect("Invalid UTF-8");
-                if state == "\n" {
-                    continue;
-                }
+                let state:String = String::from_utf8(buffer[..n]
+                    .to_vec()).expect("Invalid UTF-8");
+                if state == "\n" { continue; }
+                if state == "EOF\n" { return; }
                 print!("{state}");
-            },
+                let _ = stream.write_all(state.as_bytes());
+            }
             Err(_) => {
                 println!("Error reading Stream");
             }
