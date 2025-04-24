362191f4f004e6c6755a690789920afa3efbadc7
HeadedBranch <isaac.hesslegrave@gmail.com>
2025-04-24 23:13:08 +0100

    Update and refactor example code

--- example/src/main.rs
+++ example/src/main.rs
@@ -17,39 +17,57 @@ async fn main() {
     loop {
         // Wait for a Game to connect
         let mut balatro = remotro.accept().await.unwrap();
-
-        // Check current screen in Game
-        let screen = balatro.screen().await.unwrap();
-        match screen {
-            Screen::Menu(menu) => {
-                // Prompt the user to select Deck
-                println!("Select a deck:");
-                let mut deck = String::new();
-                std::io::stdin()
-                    .read_line(&mut deck)
-                    .expect("Failed to read line from stdin");
-                let deck_bundle = format!("{{ \"{}\": null }}", deck.trim());
-                let deck: Deck = serde_json::from_str(&deck_bundle).unwrap();
-                
-                // Prompt the user to select Stake
-                println!("Select a stake:");
-                let mut stake = String::new();
-                std::io::stdin()
-                    .read_line(&mut stake)
-                    .expect("Failed to read line from stdin");
-                let stake_bundle = format!("{{ \"{}\": null }}", stake.trim());
-                let stake: Stake = serde_json::from_str(&stake_bundle).unwrap();
-
-                let select_blind = menu.new_run(deck, stake, None).await.unwrap();
-                println!("Small blind: {:?}", select_blind.small());
-                println!("Big blind: {:?}", select_blind.big());
-                println!("Boss blind: {:?}", select_blind.boss());
-            }
-            _ => {
-                log::error!("(currently) Unimplemented state");
-                // Do another thing
+        loop{
+            // Check current screen in Game
+            let screen = balatro.screen().await.unwrap();
+            match screen {
+                Screen::Menu(menu) => {
+                    // Prompt the user to select Deck
+                    println!("Select a deck:");
+                    let mut deck = String::new();
+                    std::io::stdin()
+                        .read_line(&mut deck)
+                        .expect("Failed to read line from stdin");
+                    let deck_bundle = format!("{{ \"{}\": null }}", deck.trim());
+                    let deck: Deck = serde_json::from_str(&deck_bundle).unwrap();
+                    
+                    // Prompt the user to select Stake
+                    println!("Select a stake:");
+                    let mut stake = String::new();
+                    std::io::stdin()
+                        .read_line(&mut stake)
+                        .expect("Failed to read line from stdin");
+                    let stake_bundle = format!("{{ \"{}\": null }}", stake.trim());
+                    let stake: Stake = serde_json::from_str(&stake_bundle).unwrap();
+                    let select_blind = menu.new_run(deck, stake, None).await.unwrap();
+                }
+                Screen::Blinds(blinds) => {
+                    println!("Small blind: {:?}", blinds.small());
+                    println!("Big blind: {:?}", blinds.big());
+                    println!("Boss blind: {:?}", blinds.boss());
+                    println!("Select or skip the blind:");
+                    let mut user_input = String::new();
+                    std::io::stdin()
+                        .read_line(&mut user_input)
+                        .expect("Failed to read line from stdin");
+                    match user_input.trim().to_lowercase().as_str() {
+                        "select" => {
+                            println!("Selecting blind");
+                            blinds.select().await.unwrap();
+                            }
+                         "skip" => {
+                            println!("Skipping blind");
+                            blinds.skip().await.unwrap();
+                            }
+                         _ => {
+                            println!("Invalid input. Please enter Select or Skip.");
+                        }
+                    }
+                }
+                _ => {
+                    log::error!("(currently) Unimplemented state");
+                }
             }
         }
-        loop {}
     }
 }
