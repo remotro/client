d5d8e35b6f3900018f7be11d3cedd37d1b2cd545
HeadedBranch <isaac.hesslegrave@gmail.com>
2025-04-26 20:50:52 +0100

    Refactor logic to be clearer with errors

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,28 +1,27 @@
-use log;
-use log::error;
+use log::{debug, error, info, trace, warn};
 use remotro::{
-    Remotro,
     balatro::{
-        Screen,
         menu::{Deck, Stake},
+        Screen,
     },
+    Remotro,
 };
 use std::str::FromStr;
 
 fn get_input<T: FromStr<Err = String>>(prompt: &str) -> T {
     loop {
         println!("{prompt}");
-        let mut deck = String::new();
-        if let Err(e) = std::io::stdin().read_line(&mut deck) {
+        let mut item = String::new();
+        if let Err(e) = std::io::stdin().read_line(&mut item) {
             error!("{e}");
             continue;
         }
-        match deck.parse() {
-            Ok(deck) => return deck,
+        match item.parse() {
+            Ok(item) => return item,
             Err(e) => {
                 error!("{e}");
                 continue;
-            },
+            }
         }
     }
 }
@@ -33,48 +32,62 @@ async fn main() {
 
     // Host a TCP socket
     let mut remotro = Remotro::host("127.0.0.1", 34143).await.unwrap();
-    log::info!("Remotro hosted on 127.0.0.1:34143");
-    loop {
-        // Wait for a Game to connect
-        let mut balatro = remotro.accept().await.unwrap();
-        loop{
-            // Check current screen in Game
-            let screen = balatro.screen().await.unwrap();
-            match screen {
-                Screen::Menu(menu) => {
-                    // Prompt the user to select Deck
-                    let deck: Deck = get_input("Select Deck:");
-                    
-                    // Prompt the user to select Stake
-                    let stake: Stake = get_input("Select stake");
+    info!("Remotro hosted on 127.0.0.1:34143");
 
-                    let select_blind = menu.new_run(deck, stake, None).await.unwrap();
-                }
-                Screen::Blinds(blinds) => {
-                    println!("Small blind: {:?}", blinds.small());
-                    println!("Big blind: {:?}", blinds.big());
-                    println!("Boss blind: {:?}", blinds.boss());
-                    println!("Select or skip the blind:");
-                    let mut user_input = String::new();
-                    std::io::stdin()
-                        .read_line(&mut user_input)
-                        .expect("Failed to read line from stdin");
-                    match user_input.trim().to_lowercase().as_str() {
-                        "select" => {
-                            println!("Selecting blind");
-                            blinds.select().await.unwrap();
+    loop {
+        info!("Waiting for connection");
+        // Wait for a Game to connect
+        let mut balatro = match remotro.accept().await {
+            Ok(b) => {
+                info!("New connection accepted");
+                b
+            }
+            Err(e) => {
+                error!("Connection Failed: {e}");
+                continue;
+            }
+        };
+        loop {
+            // Check current screen in Game
+            match balatro.screen().await {
+                Ok(screen) => match screen {
+                    Screen::Menu(menu) => {
+                        // Prompt the user to select Deck
+                        let deck: Deck = get_input("Select Deck:");
+
+                        // Prompt the user to select Stake
+                        let stake: Stake = get_input("Select stake");
+
+                        menu.new_run(deck, stake, None).await.unwrap();
+                    }
+                    Screen::Blinds(blinds) => {
+                        println!("Blinds:");
+                        println!("Small blind: {:?}", blinds.small());
+                        println!("Big blind: {:?}", blinds.big());
+                        println!("Boss blind: {:?}", blinds.boss());
+                        println!("Select or skip the blind:");
+                        let mut user_input = String::new();
+                        std::io::stdin()
+                            .read_line(&mut user_input)
+                            .expect("Failed to read line from stdin");
+                        match user_input.trim().to_lowercase().as_str() {
+                            "select" => {
+                                println!("Selecting blind");
+                                blinds.select().await.unwrap();
                             }
-                         "skip" => {
-                            println!("Skipping blind");
-                            blinds.skip().await.unwrap();
+                            "skip" => {
+                                println!("Skipping blind");
+                                blinds.skip().await.unwrap();
                             }
-                         _ => {
-                            println!("Invalid input. Please enter Select or Skip.");
+                            _ => {
+                                println!("Invalid input. Please enter Select or Skip.");
+                            }
                         }
                     }
-                }
-                _ => {
-                    log::error!("(currently) Unimplemented state");
+                },
+                Err(e) => {
+                    error!("Connection Failed: {e}");
+                    break;
                 }
             }
         }
