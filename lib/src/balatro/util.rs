1773f357c058e3328338b546139f975041223d2e
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2025-05-12 00:02:34 +0100

    Correct macro to make pub enum

--- lib/src/balatro/util.rs
+++ lib/src/balatro/util.rs
@@ -2,7 +2,7 @@
 macro_rules! balatro_enum {
     ($name:ident { $($item:ident = $identifier:literal),* $(,)? }) => {
         #[derive(Serialize, Deserialize, Clone, Debug)]
-        enum $name {
+        pub enum $name {
             $(
                 #[serde(rename = $identifier)]
                 $item,
